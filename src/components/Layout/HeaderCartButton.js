b97aceb4dd66e700c173b9c715639467a0ddb34e
qpakr38 <zczc0819@gmail.com>
2022-09-04 00:52:35 +0900

    FEAT : useEffect와 timer를 활용한 이미지 효과 주기

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,35 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
-import {Fragment, useContext} from "react";
+import {Fragment, useEffect, useContext, useState} from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-    const carCtx = useContext(CartContext);
+    const [btnIsHighlighted,setBtnIsHighlighted] = useState(false);
+    const {items} = useContext(CartContext);
 
-    const numberOfCartItems = carCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount
     }, 0);
 
+
+    const btnClasses=`${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(()=>{
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(()=>{setBtnIsHighlighted(false)},300);
+
+        return () =>{
+            clearTimeout(timer);
+        }
+    },[items]);
+
     return (
         <Fragment>
-            <button className={classes.button}
+            <button className={btnClasses}
                     onClick={props.onClick}>
                 <span>
                  <CartIcon/>
