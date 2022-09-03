18d5d3066c2d9efacf41e66bb0fde81c0876d6ca
qpakr38 <zczc0819@gmail.com>
2022-09-03 12:35:02 +0900

    FEAT : MealItemForm Create

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,19 @@
 import classes from './MealItemForm.module.css'
+import Input from "../../UI/Input";
+
 const MealItemForm = () => {
-  return(
-    <form className={classes.form}>
-      <input type='text'/>
-    </form>
-  )
+    return (
+        <form className={classes.form}>
+            <Input label='Amount' input={{
+                id: 'amount',
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }}/>
+            <button> + ADD</button>
+        </form>
+    )
 };
 export default MealItemForm;
